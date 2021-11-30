4339ed772132dfb279638d5ca1d5f5c20cdb8bfc
trainingmontage <nemo.omen@gmail.com>
2021-11-30 10:06:39 -0600

    Page layout changes

--- packages/renderer/src/lib/pages/Recipes.svelte
+++ packages/renderer/src/lib/pages/Recipes.svelte
@@ -7,4 +7,12 @@
     <Icon name="book" />
     <h2>Recipes</h2>
   </div>
+  <div class="page-layout-control">
+    <button class="icon-control-button">
+      <Icon name="list" />
+    </button>
+    <button class="icon-control-button">
+      <Icon name="grid" />
+    </button>
+  </div>
 </div>
